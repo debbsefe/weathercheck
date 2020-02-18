5c5a47d3ba870af58a111062e1c4c16ad84e4a89
Mamus Eferha <eferhamamus@gmail.com>
2020-02-18 17:26:45 +0100

    'changes'

--- js/main.js
+++ js/main.js
@@ -1,47 +1,26 @@
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const msg = document.querySelector(".top-banner .msg");
-const list = document.querySelector(".ajax-section .cities");
+(function($){
+  $(document).ready(function(){
+    $('#submitBtn').click(function(){
+      var city = $('#city').val();
 
-const apiKey = "66f60778c8c27a88da3fd31a2d55d2af";
+      if(city != ''){
+          $.ajax({
+            url:'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&unit=metric' + 'APPID=66f60778c8c27a88da3fd31a2d55d2af',
+            type:'GET',
+            data: { action: 'getWeatherJson', 
+				    value:	city,      
+			            },
+            dataType:'json',
+            success:function(data){
+              console.log(data);
+            }
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const listItems = list.querySelectorAll(".ajax-section .city");
-  const inputVal = input.value;
+          });
+      }else{
+        $('#error').html('Field cannot be empty');
+      }
 
-  //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const { main, name, sys, weather } = data;
-      const icon = `https://openweathermap.org/img/wn/${
-        weather[0]["icon"]
-      }@2x.png`;
-
-      const li = document.createElement("li");
-      li.classList.add("city");
-      const markup = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
-        </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>
-      `;
-      li.innerHTML = markup;
-      list.appendChild(li);
-    })
-    .catch(() => {
-      msg.textContent = "Please search for a valid city 😩";
     });
+  });
 
-  msg.textContent = "";
-  form.reset();
-  input.focus();
 });
--- weather_check.php
+++ weather_check.php
@@ -6,37 +6,22 @@ Author: Mamus Eferha
 Version: 1.0
 */
 
-function ms_add_scripts(){
-    //CSS
-    wp_enqueue_style('ms-main-style', plugins_url(). '/weathercheck/css/style.css');
-    //JS
-    wp_enqueue_script('ms-main-script', plugins_url().'/weathercheck/js/main.js');  
-}
-add_action('wp_enqueue_scripts', 'ms_add_scripts');
+
 
 
 function weather_form(){
   ?>  
-  <form method= "post" action="" enctype="multipart/form-data"> 
-      <input type="text" name="city_id" id="city_id" placeholder="Type a city name">
-      <button type="submit">Search</button>
-      <span class="msg"></span>
-    </form>
-    <div class="ajax-section">
-      <ul class="cities">
-        <li class="city">
-          <h2 class="city-name" data-name="...">
-            <span>...</span>
-            <sup>...</sup>
-          </h2>
-          <span class="city-temp">...<sup>°C</sup></span>
-          <figure>
-            <img class="city-icon" src="..." alt="...">
-            <figcaption>...</figcaption>
-          </figure>
-        </li>
-      </ul>
+    <div class "weather-form"> 
+      <input type="text" id="city" placeholder="Type a city name">
+      <button type="submit" id="submitBtn">Search</button>
+      <span id="error"></span>
     </div>
+    <table id="weatherDisplay">
+    <tr>
+     <td>Name</td>  
+    </tr>
+    </table>
+    <div id="weather_output"></div>
   <?php
 }
 
@@ -49,3 +34,13 @@ function wpb_weather_search(){
   return $output;
 }
 add_shortcode('weathersearch', 'wpb_weather_search');
+
+function ms_add_scripts(){
+  //CSS
+  wp_enqueue_style('ms-main-style', plugins_url(). '/weathercheck/css/style.css');
+  //JS
+  wp_enqueue_script('ms-main-script', plugins_url().'/weathercheck/js/main.js', array('jquery'));  
+}
+add_action('wp_enqueue_scripts', 'ms_add_scripts');
+add_action ( 'wp_ajax_' . 'getWeatherJson', 'data');
+add_action ( 'wp_ajax_nopriv_' . 'getWeatherJson', 'data');
