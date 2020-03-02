a94bb68c6fea26c5c61d53fbc21463e9e0d2923c
Mamus Eferha <eferhamamus@gmail.com>
2020-03-02 18:28:29 +0100

    weather now outputs on frontend, still requires tweaking

--- weather_check.php
+++ weather_check.php
@@ -5,24 +5,29 @@ Description: Custom search bar
 Author: Mamus Eferha
 Version: 1.0
 */
-add_action( 'wp_ajax_ch_user_data', 'weather_display' );
-add_action( 'wp_ajax_nopriv__ch_user_data', 'weather_display' );
 
+if(! defined('ABSPATH')){
+  die('Will you get out of here!');
+}
+function ms_add_scripts(){
+  //CSS
+  wp_enqueue_style('ms-main-style', plugins_url(). '/weathercheck/css/style.css');
+  //JS
+  wp_enqueue_script('ms-main-script', plugins_url().'/weathercheck/js/main.js', array('jquery'));
+  wp_localize_script( 'ms-ajax-script', 'ajax_object',
+  array( 'ajax_url' => admin_url( 'admin-ajax.php' )));  
+}
+//add_action('wp_enqueue_scripts', 'ms_add_scripts');
 function weather_form(){
   ?>  
-    <form id="ms_form"action="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>" method="post"> 
-      <input type="hidden" name="cp_action" id="cp_action" value="ch_user_data">
-      <input type="text" id="cityId" placeholder="Type a city name">
+    <form id="ms_form" action="<?php echo ( admin_url( 'admin-ajax.php' ) ); ?>" method="post"> 
+      <input type="hidden" name="action" id="action" value="ms_weather_display">
+      <input type="text" id="cityName" name="cityName" placeholder="Type a city name">
       <input type="submit" id="submitBtn">
     </form>
+
   <?php
 }
-
-function weather_display(){
-  return "<p>test</p>";
-  wp_die();
-}
-
 function wpb_weather_search(){
   ob_start();
   weather_form();
@@ -31,14 +36,38 @@ function wpb_weather_search(){
   return $output;
 }
 add_shortcode('weathersearch', 'wpb_weather_search');
+function show_temp() {
+  $url = 'https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=66f60778c8c27a88da3fd31a2d55d2af';
+ $response = wp_remote_get( $url  );
+ if( is_array($response) ) {
+   $header = $response['headers']; // array of http header lines
+   $body = $response['body']; // use the content
+   $resp = json_decode($body);
+   $weather = $resp->weather[0]->icon;
+	  $icon = '<img src="http://openweathermap.org/img/w/' . $weather . '.png">';
+ }
+  echo '<h1>The temp in Kelvin is: </h1>'.$resp->main->temp;
+  echo $icon;
+ }
+ add_shortcode ('show_temp', 'show_temp');
 
-function ms_add_scripts(){
-  //CSS
-  wp_enqueue_style('ms-main-style', plugins_url(). '/weathercheck/css/style.css');
-  //JS
-  wp_enqueue_script('ms-main-script', plugins_url().'/weathercheck/js/main.js', array('jquery'));
-  //wp_localize_script( 'ms-main-script', 'ajax_object',
-  //array( 'ajax_url' => admin_url( 'admin-ajax.php' )));  
-}
-
-add_action('wp_enqueue_scripts', 'ms_add_scripts');
+ /*function ms_weather_display(){
+	$city = ($_POST['cityName']);
+	$apiKey = '66f60778c8c27a88da3fd31a2d55d2af';
+	$url = 'https://api.openweathermap.org/data/2.5/weather?q=' .$city. '&appid=' .$apiKey;
+	$response = wp_remote_get( $url  );
+	if( is_array($response) ) {
+	  $header = $response['headers']; // array of http header lines
+	  $body = $response['body']; // use the content
+	  $resp = json_decode($body);
+	  $weather = $resp->weather[0]->icon;
+	  $icon = '<img src="http://openweathermap.org/img/w/' . $weather . '.png">';
+ }
+  echo '<h1>The temp is: '.$resp->main->temp .'K </h1>';
+  echo $icon;
+  
+wp_die();
+  }
+  add_action( 'wp_ajax_ms_weather_display', 'ms_weather_display' );
+  add_action( 'wp_ajax_nopriv__ms_weather_display', 'ms_weather_display' );*/
+  
